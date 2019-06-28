1f4bdabc9ffccc0f82a425a150fc899cee38a756
Leony Assis <leonyra@hotmail.com>
2019-06-28 11:30:52 -0300

    removendo aarquivos de configuracao

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use leony\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new leony\DB\Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	
+	$page = new Page();
+	$page->setTpl("index");
+	
 
 });
 
